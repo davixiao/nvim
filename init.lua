97e82c238acfd7907c8d36a7bf166b6593454739
David Xiao <avidxiao@gmail.com>
2024-02-23 11:33:54 -0800

    command for unsaved buffers

--- init.lua
+++ init.lua
@@ -107,6 +107,20 @@ require('lualine').setup({
     component_separators = '|',
     section_separators = '',
   },
+	sections = {
+		lualine_z = {
+			{
+				function()
+					for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+						if vim.api.nvim_buf_get_option(buf, 'modified') then
+							return 'UNSAVED'
+						end
+					end
+					return ''
+				end,
+			},
+		},
+	},
 })
 
 -- See :help nvim-treesitter-modules
