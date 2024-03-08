da4ae2fb78106f82c618542d9a5815278192fe5c
David Xiao <avidxiao@gmail.com>
2024-03-08 15:18:45 -0800

    clean up

--- init.lua
+++ init.lua
@@ -166,31 +166,7 @@ require('nvim-treesitter.configs').setup({
 require('mini.comment').setup({})
 vim.keymap.set('n', '<C-/>', 'gcc', { remap = true })
 -- See "help MiniPairs.config
-require('mini.pairs').setup({
-  -- In which modes mappings from this `config` should be created
-  modes = { insert = true, command = false, terminal = false },
-
-  -- Global mappings. Each right hand side should be a pair information, a
-  -- table with at least these fields (see more in |MiniPairs.map|):
-  -- - <action> - one of 'open', 'close', 'closeopen'.
-  -- - <pair> - two character string for pair to be used.
-  -- By default pair is not inserted after `\`, quotes are not recognized by
-  -- `<CR>`, `'` does not insert pair after a letter.
-  -- Only parts of tables can be tweaked (others will use these defaults).
-  mappings = {
-    -- ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-    ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-    ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
-
-    -- [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
-    [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
-    ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
-
-    ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-    ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-    ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
-  },
-})
+require('mini.pairs').setup({})
 
 -- See :help telescope.builtin
 vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
@@ -264,6 +240,7 @@ cmp.setup({
 	formatting = lsp_zero.cmp_format(),
 	mapping = {
   	['<TAB>'] = cmp.mapping.select_next_item(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
 	},
 })
 -- 
